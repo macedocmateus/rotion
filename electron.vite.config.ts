9cc1c9c302f0cdc9d24bb0b173d49bd441491ed2
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-06-22 18:05:53 -0300

    fix bugs

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,8 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+
 
 export default defineConfig({
   main: {
@@ -10,11 +12,22 @@ export default defineConfig({
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
+    css: {
+      postcss: {
+        plugins: [
+          tailwindcss({
+            config: './src/renderer/tailwind.config.js'
+          })
+        ]
+      }
+    },
+    
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src')
       }
     },
+    
     plugins: [react()]
   }
 })
